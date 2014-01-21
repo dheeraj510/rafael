58414c99c1647e3dcd354fcbeef4d4d6bfd8bbf9
Ryan Bigg <radarlistener@gmail.com>
2014-01-21 12:52:38 +1100

    Reproducable example of spree/spree#3415 + rails/sass-rails#166

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rafael::Application.config.secret_key_base = 'c87d6c2cf86060790f65ce6e5c67bb4b1a0b92eef2bd0e1bec0d315401e0a36514b91fa770928b6d7340c7430b8fe79b74431a805eee94adc8428b1ed2a506b5'
---
+++ vendor/gems/spree/lib/spree.rb
@@ -0,0 +1,7 @@
+require "spree/version"
+
+module Spree
+  class Engine < Rails::Engine
+
+  end
+end
